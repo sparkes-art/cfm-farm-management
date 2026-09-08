94d53d45180d542ce72b60ef88d41f80d2036a59
sparkes-art <sparkes@cfma.com.au>
2026-09-08 11:52:17 +1000

    Update outputs.js

--- modules/outputs/outputs.js
+++ modules/outputs/outputs.js
@@ -141,7 +141,7 @@ async function _mountOverview(container) {
     // Contract position
     const contractHtml = await buildContractPosition(season);
     const opsHtml = await buildOperationsSummary(season);
-    html += contractHtml;
+    html += contractHtml + opsHtml;
 
     container.innerHTML = html;
 
